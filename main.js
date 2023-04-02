285bdb268a4009b758eab4e90f70696377a97867
khisus19 <khisus19@DESKTOP-JN9A6AK>
2023-04-01 19:23:19 -0600

    winning logic ready

--- main.js
+++ main.js
@@ -1,24 +1,63 @@
 
 
-const EXES = "❌";
+const EX = "❌";
 const OES = "⭕";
 const CHECK = "✔️";
 let isExTurn = true;
+let exesPicks = [];
+let checksPicks = [];
+const WINS = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+];
 
 
 const cell = document.querySelectorAll(".cell");
 
-console.log(cell);
 for (let i = 0; i < cell.length; i++) {
   const element = cell[i];
   element.addEventListener("click", pick)
 }
 
 function pick() {
-  let currentCell = event.target;
-  currentCell.innerHTML = isExTurn ? OES : CHECK;
+
+  if (isExTurn) {
+    this.innerHTML = EX;
+    exesPicks.push( parseInt(this.id) );
+    checkWin(exesPicks);
+  } else {
+    this.innerHTML = CHECK;
+    checksPicks.push( parseInt(this.id) );
+    checkWin(checksPicks);
+  }
   isExTurn = !isExTurn;
 
-
-  // console.log(event.target);
+  this.removeEventListener("click", pick)
 }
+
+function checkWin(arrayInTurn) {
+
+  
+  WINS.map(winPattern => {
+    const containsAll = winPattern.every(element => {
+      return arrayInTurn.includes(element);
+    })
+
+    if (containsAll) {
+      console.log("WIN!");
+
+      for (let i = 0; i < cell.length; i++) {
+        const element = cell[i];
+        element.removeEventListener("click", pick)
+      }
+    }
+
+  })
+}
+
